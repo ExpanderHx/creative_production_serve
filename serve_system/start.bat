d46d4d27bba31f04489e9c3567d498f0368b66b4
751050036@qq.com <751050036@qq.com>
2023-07-04 13:30:27 +0800

    优化

--- serve_system/start.bat
+++ serve_system/start.bat
@@ -9,17 +9,18 @@ rem echo 路径2：%~dp0
 cd /d %~dp0
 set "WINPYDIR=%~dp0\python\python-3.9.13-win"
 echo  %~dp0
+IF EXIST python (
+echo 未检测到集成环境，使用系统Python解释器
+set "PYTHON=python.exe "
+)ELSE (
+)
+
 IF EXIST %WINPYDIR% (
 echo 检测到新版集成环境，使用内置Python解释器
 set "PATH=%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%PATH%;"
 set "PYTHON=%WINPYDIR%\python.exe "
 set "PIPPATH=%~dp0\python\python-3.9.13-win\Scripts\pip.exe "
 set "USER_SITE=%~dp0\python\python-3.9.13-win\site-packages "
-)
-IF EXIST python (
-echo 未检测到集成环境，使用系统Python解释器
-set "PYTHON=python.exe "
-)ELSE (
 )
 
 rem echo %USER_SITE%
