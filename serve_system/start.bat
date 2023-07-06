4ba8bba69414a7f658c26d605f2eed5e0b4bbde8
751050036@qq.com <751050036@qq.com>
2023-07-06 11:20:12 +0800

    优化

--- serve_system/start.bat
+++ serve_system/start.bat
@@ -9,22 +9,32 @@ rem echo 路径2：%~dp0
 cd /d %~dp0
 set "WINPYDIR=%~dp0\python\python-3.9-win"
 echo  %~dp0
-IF EXIST python (
-echo 未检测到集成环境，使用系统Python解释器
-set "PYTHON=python.exe "
-)ELSE (
-)
+
 
 IF EXIST %WINPYDIR% (
-echo 检测到新版集成环境，使用内置Python解释器
-set "PATH=%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%PATH%;"
-set "PYTHON=%WINPYDIR%\python.exe "
-set "PIPPATH=%~dp0\python\python-3.9-win\Scripts\pip.exe "
-set "USER_SITE=%~dp0\python\python-3.9-win\site-packages "
+    echo 检测到内置集成环境，使用内置Python解释器
+    set "PATH=%WINPYDIR%\;%WINPYDIR%\DLLs;%WINPYDIR%\Scripts;%PATH%;"
+    set "PYTHON=%WINPYDIR%\python.exe "
+    set "PIPPATH=%~dp0\python\python-3.9-win\Scripts\pip.exe "
+    set "USER_SITE=%~dp0\python\python-3.9-win\site-packages "
+)ELSE (
+    echo 未检测到内置集成环境，使用系统Python解释器
+    set "PYTHON=python.exe "
 )
 
+set "REQUIREMENTS_FILE=..\requirements-win-cpu.txt"
+
+SET "TORCH_GPU_FILE=%~dp0pip_packages\torch-2.0.1+cu117-cp39-cp39-win_amd64.whl"
+echo %TORCH_GPU_FILE%
+if exist %TORCH_GPU_FILE% (
+   set "REQUIREMENTS_FILE=..\requirements-win-gpu.txt"
+) else (
+    echo %TORCH_GPU_FILE% is not exist!
+)
+echo %REQUIREMENTS_FILE%
+
 rem echo %USER_SITE%
-%PIPPATH% install --no-index --find-links=./pip_packages -r ../requirements.txt
+%PIPPATH% install --no-index --find-links=./pip_packages -r %REQUIREMENTS_FILE%
 
 %PYTHON% ../api.py
 rem %PYTHON% E:\pycharm\creative_production_serve\api.py
